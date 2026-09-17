43c2d3c2146bea5d6ebc68badd7289f527bee86c
Keshavvenisetty <keshavvenisetty.com>
2026-09-17 16:19:38 +0530

    vpc-module

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@ module "vpc" {
     source = "git::https://github.com/daws-84s/terraform-aws-vpc.git?ref=main"
     project = var.project
     environment = var.environment
-    public_subnet_cidr = var.public_subnet_cidr
-    private_subnet_cidr = var.private_subnet_cidr
-    database_subnet_cidr = var.database_subnet_cidr
+    public_subnet_cidrs = var.public_subnet_cidrs
+    private_subnet_cidrs = var.private_subnet_cidrs
+    database_subnet_cidrs = var.database_subnet_cidrs
     is_peering_required = true
 }
